1efa6c9d791f06bf0ff5ad08bae82cd69c2420d8
madhuribajaj <immadhurigupta@gmail.com>
2023-01-28 13:17:02 +0530

    Basic Task

---
+++ Demo/src/mypackage/MyClass.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		System.out.println("TestBase");
+		System.out.println("PageLayers");
+		System.out.println("Utilitize");
+		System.out.println("PageTest");
+		System.out.println("Suite");
+
+	}
+
+}
